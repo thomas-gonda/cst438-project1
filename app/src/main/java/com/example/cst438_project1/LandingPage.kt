2be7642549ff0933a8b0f4c5ee102827ef693b6c
Adrik <adrikrenteria@gmail.com>
2026-09-02 07:35:38 -0700

    changed the layout so it fills the screen and the text is centered

--- app/src/main/java/com/example/cst438_project1/LandingPage.kt
+++ app/src/main/java/com/example/cst438_project1/LandingPage.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cst438_project1.ui.theme.Cst438project1Theme
@@ -21,7 +24,7 @@ class LandingPage : ComponentActivity() {
             Cst438project1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting2(
-                        name = "Android",
+                        name = "User",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -32,16 +35,27 @@ class LandingPage : ComponentActivity() {
 
 @Composable
 fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(
+        //changes the layout on screen
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Welcome $name!",
+            modifier = modifier
+        )
+        Text(
+            text = "It's Tipsy Time! "
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview2() {
     Cst438project1Theme {
-        Greeting2("Android")
+        //eventually will be the username from the DB
+        Greeting2("User")
     }
 }
