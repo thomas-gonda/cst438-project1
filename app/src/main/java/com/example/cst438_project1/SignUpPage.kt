2b408c71077e75bd60703c2e548885e0bfd229da
Adrik <adrikrenteria@gmail.com>
2026-09-22 09:30:14 -0700

    created the basic outline for the sign up page

--- app/src/main/java/com/example/cst438_project1/SignUpPage.kt
+++ app/src/main/java/com/example/cst438_project1/SignUpPage.kt
@@ -1,16 +1,33 @@
 package com.example.cst438_project1
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.focusable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.cst438_project1.ui.theme.Cst438project1Theme
 
 class SignUpPage : ComponentActivity() {
@@ -20,28 +37,80 @@ class SignUpPage : ComponentActivity() {
         setContent {
             Cst438project1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+                    SignUP(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun SignUP(modifier: Modifier = Modifier) {
+    // Variables for user data
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var firstname by remember { mutableStateOf("") }
+    var lastname by remember { mutableStateOf("") }
+    val coroutineScope = rememberCoroutineScope()
+    val context = LocalContext.current
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Create New Account")
+
+        Spacer(modifier = Modifier.height(16.dp))
+        // Username text box
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("Username") }
+        )
+        //first name text box
+        OutlinedTextField(
+            value = firstname,
+            onValueChange = { firstname = it },
+            label = { Text("First Name") },
+            visualTransformation = PasswordVisualTransformation() // Hides entered password
+        )
+        //first name text box
+        OutlinedTextField(
+            value = lastname,
+            onValueChange = { lastname = it },
+            label = { Text("Last Name") },
+            visualTransformation = PasswordVisualTransformation() // Hides entered password
+        )
+
+        // Password text box
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            visualTransformation = PasswordVisualTransformation() // Hides entered password
+        )
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Confirm Password") },
+            visualTransformation = PasswordVisualTransformation() // Hides entered password
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            Toast.makeText(context, "Sign Up clicked!", Toast.LENGTH_SHORT).show()
+        }) {Text("Sign Up")}
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
+fun SignUpPreview() {
     Cst438project1Theme {
-        Greeting("Android")
+        SignUP()
     }
 }
